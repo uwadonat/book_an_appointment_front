72ba8eb9591e628fe4324256413c8508d4727dea
uwadonat <uwadonat@gmail.com>
2021-11-05 20:17:34 +0300

    Initial set up

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,7 +1,35 @@
+import { useSelector, useDispatch } from "react-redux";
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { getDoctors } from "../redux/Doctors";
+
 const Home = () => {
+  const doctors = useSelector((state) => state.doctorReducer);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (!doctors.length) {
+      dispatch(getDoctors());
+    }
+  }, []);
+
   return (
     <div className="home-container">
       <div className="card">
+        <div>
+          {doctors.map((doctor) => (
+            <div className="col-12 col-md-6 col-lg-4">
+              <div className="doctor">
+                <div className="circle">
+                  <img src={doctor.image} alt="doctors" />
+                </div>
+                <h2 className="name">
+                  <Link to={`/DetailsPage/${doctor.id}`}>{doctor.name}</Link>
+                </h2>
+              </div>
+            </div>
+          ))}
+        </div>
         <img className="card-img-top" alt="imagee" />
         <div className="card-body">
           <h5 className="card-title">Card title</h5>
